4ea287c001c22fa25840dcf389ed011c840d90ac
Kabuto0421 <kabutoyudai@gmail.com>
2024-04-06 18:02:13 +0900

    googleMap and invisibleError add

---
+++ src/libs/components/Map.tsx
@@ -0,0 +1,46 @@
+/*Since the map was loaded on client side, 
+we need to make this component client rendered as well*/
+'use client'
+
+//Map component Component from library
+import { GoogleMap } from "@react-google-maps/api";
+
+//Map's styling
+const defaultMapContainerStyle = {
+    width: '100%',
+    height: '500px',
+    borderRadius: '15px 0px 0px 15px',
+};
+
+//K2's coordinates
+const defaultMapCenter = {
+    lat: 36.561325,
+    lng: 136.656205
+}
+
+//Default zoom level, can be adjusted
+const defaultMapZoom = 13
+
+//Map options
+const defaultMapOptions = {
+    zoomControl: true,
+    tilt: 0,
+    gestureHandling: 'auto',
+    mapTypeId: 'satellite',
+};
+
+const MapComponent = () => {
+    return (
+        <div>
+            <GoogleMap
+                mapContainerStyle={defaultMapContainerStyle}
+                center={defaultMapCenter}
+                zoom={defaultMapZoom}
+                options={defaultMapOptions}
+            >
+            </GoogleMap>
+        </div>
+    )
+};
+
+export { MapComponent };
---
+++ src/providers/map-provider.tsx
@@ -0,0 +1,26 @@
+//Since the map will be laoded and displayed on client side
+'use client';
+
+// Import necessary modules and functions from external libraries and our own project
+import { Libraries, useJsApiLoader } from '@react-google-maps/api';
+import { ReactNode } from 'react';
+
+// Define a list of libraries to load from the Google Maps API
+const libraries = ['places', 'drawing', 'geometry'];
+
+// Define a function component called MapProvider that takes a children prop
+export function MapProvider({ children }: { children: ReactNode }) {
+
+    // Load the Google Maps JavaScript API asynchronously
+    const { isLoaded: scriptLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: "AIzaSyBiHBkYrPgCds4ZjiNOJKYjxl90VzJvVns",
+        libraries: libraries as Libraries,
+    });
+
+    if (loadError) return <p>Encountered error while loading google maps</p>
+
+    if (!scriptLoaded) return <p>Map Script is loading ...</p>
+
+    // Return the children prop wrapped by this MapProvider component
+    return <>{children}</>;
+}
