1f0d402742dbbfc5c715e67735f988c2c6f0fc58
Kabuto0421 <kabutoyudai@gmail.com>
2024-04-10 11:00:48 +0900

    ui adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,126 @@
 'use client'
 import React, { useState } from 'react';
-import { Button, Container, Typography, Grid, TextField, InputAdornment, Box } from '@mui/material';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { Button, Container, Typography, Grid, Box } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import HistoryIcon from '@mui/icons-material/History';
 import DnsIcon from '@mui/icons-material/Dns';
 import MapProvider from "@/providers/map-provider";
 import MapComponent from "../libs/components/Map";
-export default function Home() {
 
+export default function Home() {
     const [mapVisible, setMapVisible] = useState(false);
 
     const handleStartButtonClick = () => {
         setMapVisible(true); // mapVisibleをtrueに更新する
     };
+
+    // アイコンのスタイル
+    const iconStyle = {
+        fontSize: '64px', // アイコンのサイズを大きくする
+    };
+
     return (
-
         <Container maxWidth="sm">
-            <Grid container spacing={2} style={{ padding: '20px 0' }}>
-                {/* スコア表示のBox */}
-                <Grid item xs={12}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        padding={2}
-                        bgcolor="#424242"
-                    >
-                        <Typography variant="h6" color="common.white">
-                            Score:
-                        </Typography>
-                        <TextField
-                            InputProps={{
-                                startAdornment: <InputAdornment position="start" />,
-                                style: { color: '#fff' },
+            <Grid item xs={12}>
+                <Grid container spacing={2} style={{ padding: '20px 0' }}>
+                    {/* スコア表示部分 */}
+                    <Grid item xs={9}>
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            padding={2}
+                            bgcolor="#F1D3B1"
+                            sx={{ borderRadius: '16px', textAlign: 'center', width: '100%' }}
+                        >
+                            <Typography
+                                variant="h3"
+                                component="div"
+                                sx={{
+                                    fontWeight: 'bold',
+                                    color: 'black',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    width: '100%'
+                                }}
+                            >
+                                Score: <Box component="span" sx={{ marginLeft: 1 }}>10000000</Box>
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    {/* Personアイコン表示部分 */}
+                    <Grid item xs={2.5}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            size="large"
+                            sx={{
+                                backgroundColor: "#F1D3B1",
+                                color: "black",
+                                fontSize: '20px', // テキストのサイズを大きくする
+                                // アイコンのスタイルを直接ここに適用することもできる
+                                '& .MuiSvgIcon-root': { fontSize: iconStyle.fontSize },
                             }}
-                            variant="standard"
-                            value="10000000"
-                            disabled
-                        />
-                    </Box>
-
+                            centerRipple={true} // リップル効果を中心から
+                        >
+                            <PersonIcon /> {/* ボタンの子要素としてアイコンを直接配置 */}
+                        </Button>
+                    </Grid>
                 </Grid>
                 {/* 3つのボタンを横並び */}
                 <Grid item xs={12} container spacing={0} justifyContent="center">
+                    { /* ボタンのスタイルを統一して適用 */}
                     <Grid item xs={4} padding={1}>
-                        <Button variant="contained" startIcon={<DnsIcon />} fullWidth size="large">
-                            RANKING
+                        <Button
+                            variant="contained"
+                            startIcon={<DnsIcon style={iconStyle} />}
+                            fullWidth
+                            size="large"
+                            sx={{
+                                backgroundColor: "#F1D3B1",
+                                color: "black",
+                                fontSize: '20px', // テキストのサイズを大きくする
+                            }}
+                        >
+
                         </Button>
                     </Grid>
                     <Grid item xs={4} padding={1}>
-                        <Button variant="contained" startIcon={<SportsEsportsIcon />} fullWidth size="large">
-                            GAME
+                        <Button
+                            variant="contained"
+                            startIcon={<SportsEsportsIcon style={iconStyle} />}
+                            fullWidth
+                            size="large"
+                            sx={{
+                                backgroundColor: "#F1D3B1",
+                                color: "black",
+                                fontSize: '20px', // テキストのサイズを大きくする
+                            }}
+                        >
+
                         </Button>
                     </Grid>
                     <Grid item xs={4} padding={1}>
-                        <Button variant="contained" startIcon={<AddPhotoAlternateIcon />} fullWidth size="large">
-                            PICTURE
+                        <Button
+                            variant="contained"
+                            startIcon={<HistoryIcon style={iconStyle} />}
+                            fullWidth
+                            size="large"
+                            sx={{
+                                backgroundColor: "#F1D3B1",
+                                color: "black",
+                                fontSize: '20px', // テキストのサイズを大きくする
+                            }}
+                        >
+
                         </Button>
                     </Grid>
                 </Grid>
                 {/* 条件付きレンダリングでMapProviderとStartボタンの表示を切り替える */}
                 {mapVisible ? (
-                    //map表示用のコンポーネント
                     <MapProvider>
                         <MapComponent />
                     </MapProvider>
@@ -72,30 +130,32 @@ export default function Home() {
                             display="flex"
                             alignItems="center"
                             justifyContent="center"
-                            bgcolor="#eee"
+                            bgcolor="#F1D3B1"
                             style={{ height: '500px' }}
+                            sx={{ borderRadius: '16px' }}
                         >
-                            {/* StartボタンにonClickイベントハンドラを追加 */}
-                            <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />} onClick={handleStartButtonClick}>
+                            <Button
+                                variant="contained"
+                                startIcon={<PlayArrowIcon style={iconStyle} />}
+                                onClick={handleStartButtonClick}
+                                sx={{
+                                    fontSize: '40px', // テキストのサイズを大きくする
+                                    backgroundColor: '#55645D', // ボタンの背景色を設定
+                                    color: 'white', // ボタンのテキスト色を白にする
+                                    '&:hover': {
+                                        backgroundColor: 'rgba(85, 100, 93, 0.8)', // ホバー時の背景色を少し明るくする
+                                    },
+                                }}
+                            >
                                 Start
                             </Button>
                         </Box>
                     </Grid>
                 )}
-                {/* 下部ボタン二つ */}
-                <Grid item xs={6}>
-                    <Button variant="contained" fullWidth size="large">
-                        Button 1
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button variant="contained" fullWidth size="large">
-                        Button 2
-                    </Button>
-                </Grid>
             </Grid>
-
+            <Grid item xs={12}>
+                <Box style={{ height: '100px' }}></Box>
+            </Grid>
         </Container>
-
     );
 }
