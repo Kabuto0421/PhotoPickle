48e95ba6746515be7171e6450cc5b276f195662f
Tayu <yukitake844@gmail.com>
2024-05-04 21:46:08 +0900

    user auth setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NextAuthProvider from '@/providers/NextAuth'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
+      </body>
     </html>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 import { getAuthSession } from "@/libs/nextauth"
-import Signup from "@/libs/components/auth/Signup"
+import Signup from "@/libs/components/Signup"
 
 // アカウント仮登録ページ
 const SignupPage = async () => {
