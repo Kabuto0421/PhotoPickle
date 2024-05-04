35c2327614d10b0dcbed8929e6afbb333095d26a
Tayu <yukitake844@gmail.com>
2024-05-04 22:55:05 +0900

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,28 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-  // 認証が不要なパス
-  if (pathname === '/login') {
+    // 認証が不要なパス（静的ファイル、APIルート）
+    if (pathname.includes('/_next') || pathname.includes('/api')) {
+        return NextResponse.next();
+    }
+
+    const sessionToken = request.cookies.get('next-auth.session-token');
+
+    // ログインページにアクセスし、既に認証済みの場合、トップページにリダイレクト
+    if (pathname === '/login' && sessionToken) {
+        const url = request.nextUrl.clone();
+        url.pathname = '/';
+        return NextResponse.redirect(url);
+    }
+
+    // 認証済みかチェック
+    if (!sessionToken && pathname !== '/login') {
+        const url = request.nextUrl.clone();
+        url.pathname = '/login';
+        return NextResponse.redirect(url);
+    }
+
     return NextResponse.next();
-  }
-
-  // セッションやクッキーをチェック
-  const sessionToken = request.cookies.get('next-auth.session-token');
-
-  if (pathname === '/login' && sessionToken) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/';
-    return NextResponse.redirect(url);
 }
-
-  // 認証済みかチェック
-  if (!sessionToken) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
-
