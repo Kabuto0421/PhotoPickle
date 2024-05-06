5cd609dd869f7436147a1eee3bdf96cccc11960a
Tayu <yukitake844@gmail.com>
2024-05-06 22:07:51 +0900

    login logout page

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,30 +1,35 @@
 'use client'
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { Button, Card, CardContent, Container, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 
-const LoginPage = () => {
+const LogoutPage = () => {
   const { data: session } = useSession();
 
   return (
     <div>
       {session && (
-        <div>
-          <div>
-            ログアウトしますか？
-          </div>
-          <button onClick={() => signOut()}>
-            ログアウト
-          </button>
-          <button>
-            <Link href="/profile">
-              キャンセル
-            </Link>
-          </button>
-        </div>
+        <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+          <Card sx={{ maxWidth: 300 }}>
+            <CardContent sx={{ textAlign: 'center' }}>
+              <Typography variant="h6" fontWeight="fontWeightBold">
+                ログアウトしますか？
+              </Typography>
+              <Button variant="contained" sx={{ mt: 2, mr:2 }} onClick={() => signOut()}>
+                はい
+              </Button>
+              <Button variant="outlined" sx={{ mt: 2 }} >
+                <Link href='/profile'>
+                  キャンセル
+                </Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
       )}
     </div>
   );
 }
 
-export default LoginPage;
+export default LogoutPage;
