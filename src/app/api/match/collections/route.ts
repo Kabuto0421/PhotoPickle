2b9b156745ad74be1d2c13160b112682aaadd03c
Kabuto <84449032+Kabuto0421@users.noreply.github.com>
2024-05-09 13:29:20 +0900

    サンプルレスポンスを加えた (#87)

--- src/app/api/match/collections/route.ts
+++ src/app/api/match/collections/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const GET = async (req: Request) => {
+  // ログを出力
   console.log("今まで撮った画像一覧API");
-}; 
+
+  // サンプルのレスポンスを返す
+  return NextResponse.json({ message: "今まで撮った画像一覧API" });
+};
--- src/app/api/match/route.ts
+++ src/app/api/match/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const GET = async (req: Request) => {
+  // ログを出力
   console.log("画像入力API");
+
+  // サンプルのレスポンスを返す
+  return NextResponse.json({ message: "画像入力API" });
 };
