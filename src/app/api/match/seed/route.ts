521e1a38509a52e712fdc218e04ea84783a850bb
Tayu <yukitake844@gmail.com>
2024-05-09 23:42:03 +0900

    seed routes

--- src/app/api/match/seed/route.ts
+++ src/app/api/match/seed/route.ts
@@ -1,20 +1,52 @@
-import { NextResponse } from "next/server";
+// seed値からマーカーの緯度,マーカーの経度を取得するAPI
 
-export const GET = async (req: Request) => {
-  // ログを出力
-  console.log("マーカーの緯度経度を取得するAPI");
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-  // サンプルのレスポンスを返す
-  return NextResponse.json({ message: "マーカーの緯度経度を取得するAPI" });
-};
+const prisma = new PrismaClient();
 
-export const POST = async (req: Request) => {
-  // リクエストの内容を取得
-  const data = await req.json();
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (e) {
+    return Error("DB接続エラー");
+  }
+  return 
+}
 
-  // ログを出力
-  console.log("マーカーの緯度経度、seed値を登録するAPI", data);
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  try {
+    await main();
+    const data = await prisma.match.findMany({
+      select: {
+        id: true,
+        matchPins: {
+          select: {
+            id: true,
+            latitude: true,
+            longitude: true,
+        }
+      },
+    }, 
+  })
+    return NextResponse.json(data, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ message: "DBエラー" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
 
-  // サンプルのレスポンスを返す
-  return NextResponse.json({ message: "マーカーの緯度経度、seed値を登録するAPI" });
-};
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const { Seed, latitude, longitude } = await req.json();
+    await main();
+    const match = await prisma.match.create({ data: { Seed, latitude, longitude } });
+    return NextResponse.json({ message: "success" }, { status: 201 });
+  } catch (e) {
+    return Error("DBエラー");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
