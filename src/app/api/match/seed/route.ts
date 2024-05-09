71c6dbf405d30cb9b8df5d9299cac7709fd0f371
Kabuto0421 <kabutoyudai@gmail.com>
2024-05-09 13:25:50 +0900

    サンプルレスポンスを加えた

--- src/app/api/match/seed/route.ts
+++ src/app/api/match/seed/route.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const GET = async (req: Request) => {
+  // ログを出力
   console.log("マーカーの緯度経度を取得するAPI");
-}
 
-export const POST = async (req: Request, res: NextResponse) => {
-  console.log("マーカーの緯度経度、seed値を登録するAPI");
-}
+  // サンプルのレスポンスを返す
+  return NextResponse.json({ message: "マーカーの緯度経度を取得するAPI" });
+};
+
+export const POST = async (req: Request) => {
+  // リクエストの内容を取得
+  const data = await req.json();
+
+  // ログを出力
+  console.log("マーカーの緯度経度、seed値を登録するAPI", data);
+
+  // サンプルのレスポンスを返す
+  return NextResponse.json({ message: "マーカーの緯度経度、seed値を登録するAPI" });
+};
--- src/app/api/python/route.ts
+++
@@ -1,4 +0,0 @@
-import { NextResponse } from 'next/server'
-export async function POST(req: Request) {
-    const { image1_data, image2_data } = await req.json();
-}
