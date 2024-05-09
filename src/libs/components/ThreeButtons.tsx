c5b9244e3b86606f62560e2eeb76fe9b2b392488
Kabuto0421 <kabutoyudai@gmail.com>
2024-05-09 22:00:04 +0900

    createページマップ以外は完成

--- src/libs/components/ThreeButtons.tsx
+++ src/libs/components/ThreeButtons.tsx
@@ -5,7 +5,7 @@ import { Grid, Button } from '@mui/material';
 import DnsIcon from '@mui/icons-material/Dns';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import HistoryIcon from '@mui/icons-material/History';
-
+import Link from 'next/link';
 // ThreeButtonsコンポーネントのpropsの型を定義する。
 interface ThreeButtonsProps {
     iconStyle: CSSProperties; // アイコンに適用されるスタイルの型。
@@ -18,19 +18,21 @@ export default function ThreeButtons({ iconStyle }: ThreeButtonsProps) {
         <Grid container spacing={0} justifyContent="center" alignItems="center" paddingTop={1}>
             {/* DNSアイコンボタン */}
             <Grid item xs={4} paddingRight={1} sx={{ height: '100%' }}>
-                <Button
-                    variant="contained" // ボタンの外観を塗りつぶしに設定。
-                    startIcon={<DnsIcon style={iconStyle} />} // アイコンとしてDnsIconを使用し、propsから受け取ったスタイルを適用。
-                    fullWidth // ボタンの幅をGridアイテムの幅いっぱいに設定。
-                    sx={{
-                        backgroundColor: "#437b8d", // ボタンの背景色を設定。
-                        color: "white", // ボタンのテキスト色を設定。
-                        '&:hover': {
-                            backgroundColor: 'rgba(241, 172, 23, 0.8)', // ホバー時の背景色を少し明るくする
-                            color: 'black'
-                        },
-                    }}
-                />
+                <Link href="create">
+                    <Button
+                        variant="contained" // ボタンの外観を塗りつぶしに設定。
+                        startIcon={<DnsIcon style={iconStyle} />} // アイコンとしてDnsIconを使用し、propsから受け取ったスタイルを適用。
+                        fullWidth // ボタンの幅をGridアイテムの幅いっぱいに設定。
+                        sx={{
+                            backgroundColor: "#437b8d", // ボタンの背景色を設定。
+                            color: "white", // ボタンのテキスト色を設定。
+                            '&:hover': {
+                                backgroundColor: 'rgba(241, 172, 23, 0.8)', // ホバー時の背景色を少し明るくする
+                                color: 'black'
+                            },
+                        }}
+                    />
+                </Link>
             </Grid>
             {/* スポーツエスポーツアイコンボタン */}
             <Grid item xs={4} padding={0} sx={{ height: '100%' }}>
