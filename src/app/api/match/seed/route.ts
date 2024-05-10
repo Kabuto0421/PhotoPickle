842706d1f0e0ff7930c2e3a837d0aaf15a37a5b3
Kabuto0421 <kabutoyudai@gmail.com>
2024-05-10 09:44:45 +0900

    main削除

--- src/app/api/match/seed/route.ts
+++ src/app/api/match/seed/route.ts
@@ -1,22 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse, NextRequest } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function main() {
-  try {
-    await prisma.$connect();
-  } catch (e) {
-    console.error('DB接続エラー', e);
-    return Error('DB接続エラー');
-  }
-  return;
-}
-
 export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await main();
+    await prisma.$connect();
     const data = await prisma.match.findMany({
       select: {
         id: true,
@@ -30,11 +19,9 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     });
     res.status(200).json(data);
-    return NextResponse.json(data, { status: 200 });
   } catch (e) {
     console.error('DBエラー', e);
     res.status(500).json({ message: 'DBエラー' });
-    return NextResponse.json({ message: 'DBエラー' }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
@@ -43,11 +30,11 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
 export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { Seed, matchPins, userId }: { Seed: string; matchPins: { latitude: number; longitude: number }[]; userId: string } = req.body;
-    await main();
+    await prisma.$connect();
     const match = await prisma.match.create({
       data: {
         Seed,
-        userId, // userId をデータに追加
+        userId,
         matchPins: {
           createMany: {
             data: matchPins.map((pin) => ({
@@ -59,11 +46,9 @@ export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     });
     res.status(201).json({ message: 'success' });
-    return NextResponse.json({ message: 'success' }, { status: 201 });
   } catch (e) {
     console.error('DBエラー', e);
     res.status(500).json({ message: 'DBエラー' });
-    return NextResponse.json({ message: 'DBエラー' }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
