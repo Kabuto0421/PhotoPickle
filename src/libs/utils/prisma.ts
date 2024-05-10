fc13cb815f1200670f7801f0766dcfb61fdb25cf
Kabuto0421 <kabutoyudai@gmail.com>
2024-05-10 09:55:53 +0900

    utils/prisma.ts fix

--- src/libs/utils/prisma.ts
+++ src/libs/utils/prisma.ts
@@ -2,21 +2,27 @@ import { PrismaClient } from '@prisma/client';
 import { User } from '../types';
 
 const prisma = new PrismaClient();
+// types.ts
+interface SimpleUser {
+  id: string;
+  name: string | null;
+  email: string | null;
+  image: string | null;
+}
+export async function getUserById(userId: string): Promise<SimpleUser | null> {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { id: true, name: true, email: true, image: true },
+  });
 
-export async function getUserById(userId: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      select: { id: true, name: true, email:true, image: true },
-    });
+  if (!user) {
+    return null;
+  }
 
-    if (!user) {
-      return null;
-    }
-
-    return {
-      id: user.id,
-      name: user.name ?? null,
-      email: user.email ?? null,
-      image: user.image ?? null,
-    };
+  return {
+    id: user.id,
+    name: user.name ?? null,
+    email: user.email ?? null,
+    image: user.image ?? null,
+  };
 }
